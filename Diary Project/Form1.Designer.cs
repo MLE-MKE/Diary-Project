f557ec8571110af22056fd06c2f82b897a93560e
MLE-MKE <easy_mac55@ymail.com>
2024-02-24 22:45:19 -0600

    Add project files.

---
+++ Diary Project/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Diary_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Journal = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // Journal
+            // 
+            this.Journal.Location = new System.Drawing.Point(13, 32);
+            this.Journal.Multiline = true;
+            this.Journal.Name = "Journal";
+            this.Journal.Size = new System.Drawing.Size(775, 406);
+            this.Journal.TabIndex = 0;
+            this.Journal.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(13, 3);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // NewButton
+            // 
+            this.NewButton.Location = new System.Drawing.Point(95, 3);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(75, 23);
+            this.NewButton.TabIndex = 2;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.Click += new System.EventHandler(this.NewButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Journal);
+            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "Form1";
+            this.Text = "Journal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Journal;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+    }
+}
+
---
+++ Diary Project/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Diary_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save Diary Entry";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the file path chosen by the user
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    // Write the contents of the TextBox to the chosen file
+                    File.WriteAllText(filePath, Journal.Text);
+                    MessageBox.Show("Diary entry saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving diary entry: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+
+            // Check if the TextBox is empty
+            if (Journal.Text != "")
+            {
+                // Ask the user if they want to save the current entry
+                DialogResult result = MessageBox.Show("Do you want to save the current entry before starting a new one?", "Save Entry", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Save the current entry
+                    SaveEntry();
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    // If the user cancels, return without clearing the TextBox
+                    return;
+                }
+            }
+
+            // Clear the TextBox to start a new diary entry
+            Journal.Clear();
+        }
+
+        private void SaveEntry()
+        {
+            // Prompt the user to choose a location to save the diary entry
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Save Diary Entry";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the file path chosen by the user
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    // Write the contents of the TextBox to the chosen file
+                    File.WriteAllText(filePath, Journal.Text);
+                    MessageBox.Show("Diary entry saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving diary entry: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+        }
+
+
+    }
+}
